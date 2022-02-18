3babcb958fb4c4e81c036f2a762de8e0fc3d83ea
Asmaa <Asmaa99musse@gmail.com>
2022-02-18 23:46:41 +0000

    creating data structure for the projects

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "Readers Revival",
+    subtitle: "HTML, JavaScript, Web APIs, Moment.js",
+    description:
+      "Reading planner application where the user can search for a specific book. The user can add books to their planner and view scheduled books per month.",
+    image: "./assets/project-pics/readers-revival.jpg",
+    github: "https://github.com/AsmaaMusse/readers-revival",
+    application: "https://asmaamusse.github.io/readers-revival/",
+  },
+
+  {
+    title: "Food4U",
+    subtitle: "Node.js, Express.js, MySQL, Handlebars",
+    description:
+      "An application using the MVC paradigm, with its own server-side API, user authentication, and which connects to a database",
+    image: "./assets/project-pics/mobile-home-page.png",
+    github: "https://github.com/AsmaaMusse/food4U",
+    application: "https://food-4u.herokuapp.com/",
+  },
+
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    github: "https://reactbootcamp.com",
+    application: "",
+  },
+
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+];

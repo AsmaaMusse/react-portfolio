6c52d7aed1762e42909ad44d8f91e821aed900c4
Asmaa <Asmaa99musse@gmail.com>
2022-02-18 23:46:00 +0000

    completed the about me section

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,4 @@
 import picture from "../assets/images/Asmaa.jpg";
-import email from "../assets/images/twotone_email_white_24dp.png";
 import "../App.css";
 
 export default function About() {
@@ -8,7 +7,7 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Asmaa Musse.
+            Hi, I'm ASMAA MUSSE.
             <br className="hidden lg:inline-block" />
             Full-Stack Web Developer.
           </h1>
@@ -21,29 +20,22 @@ export default function About() {
 
           <div className="flex justify-center">
             <a
-              href="#contacts"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
-            >
-              View Contacts
-            </a>
-
-            <a
               href="https://docs.google.com/document/d/169kEqUcNLwGPWIW2tqN7GuHnhUUTBz_wP80cJCaL5Mg/edit?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
             >
               View Resume
             </a>
           </div>
         </div>
 
-        <div className="flex flex-wrap justify-center">
-          <div className="w-6/12 sm:w-4/12 px-4">
+        <div class="flex flex-wrap justify-center">
+          <div class="w-7/12 sm:w-5/12 px-4">
             <img
-              src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
-              alt="..."
-              className="shadow rounded-full max-w-full h-auto align-middle border-none"
+              src={picture}
+              alt="profile picture"
+              class="shadow-lg rounded max-w-full h-auto align-middle border-none"
             />
           </div>
         </div>

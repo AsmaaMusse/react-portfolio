09947cd2aa44223bcc3aac6e7647c4d186be2653
Asmaa <Asmaa99musse@gmail.com>
2022-02-20 18:41:36 +0000

    completed section for skills

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ export default function App() {
     <main className="text-gray-400 bg-gray-900 body-font">
       <About />
       <Projects />
-      {/* 
-      <Navbar />
       <Skills />
+      {/* 
       
+      <Navbar />
       <Contacts /> */}
     </main>
   );

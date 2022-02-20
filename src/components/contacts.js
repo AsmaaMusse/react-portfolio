6565b190f8375e479beade1a1f2f259dcf9da980
Asmaa <Asmaa99musse@gmail.com>
2022-02-20 22:02:26 +0000

    working on contact form connecting to gmail

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,85 +1,143 @@
 import React from "react";
+import { Form, Input, TextArea, Button } from "semantic-ui-react";
+import "../App.css";
+
+// const SERVICE_ID = "service_9ssihkh",
+// const TEMPLATE_ID = "template_hy4uhzm",
+// const USER_ID = "",
 
 export default function Contacts() {
-  return (
-    <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <form
-          name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col w-full md:py-8 mt-8 md:mt-0"
+  // return (
+  //   <section role="form" class="php-email-form" id="contact-form">
+  //     <div class="form-row">
+  //       <div class="form-group col-md-6">
+  //         <label for="name">Name:</label>
+  //         <input
+  //           type="text"
+  //           name="user_name"
+  //           class="form-control"
+  //           id="name"
+  //           data-rule="minlen:4"
+  //           data-msg="Please enter at least 3 characters"
+  //         />
+  //         <div class="validate"></div>
+  //       </div>
+  //       <br />
+  //       <div class="form-group col-md-6">
+  //         <label for="name">Enter your email below:</label>
+  //         <input
+  //           type="email"
+  //           name="email"
+  //           class="form-control"
+  //           id="email"
+  //           data-rule="email"
+  //           data-msg="Please enter a valid email"
+  //         />
+  //         <div class="validate"></div>
+  //       </div>
+  //     </div>
+  //     <br />
+  //     <div class="form-group">
+  //       <label for="name">Message</label>
+  //       <textarea
+  //         class="form-control"
+  //         name="message"
+  //         rows="10"
+  //         id="message"
+  //         data-rule="required"
+  //         data-msg="Message:"
+  //       ></textarea>
+  //       <div class="validate"></div>
+  //     </div>
+  //     <div class="mb-3">
+  //       <div class="loading">Loading</div>
+  //     </div>
+  //     <div class="text-center">
+  //       <button id="sendButton" onclick="sendMail()" value="Send" type="submit">
+  //         Send Message
+  //       </button>
+  //     </div>
+  //   </section>
+  // );
+
+  <form class="w-full max-w-lg">
+    <div class="flex flex-wrap -mx-3 mb-6">
+      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <label
+          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          for="grid-first-name"
         >
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
-          </h2>
-          <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
-          </p>
-
-          <div className="relative mb-4">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-300">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              s
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </div>
-
-          <label
-            for="email-adress-icon"
-            class="block mb-2 text-sm font-medium text-gray-400 text-gray-300"
-          >
-            Your Email
-          </label>
-          <div class="relative mt-1">
-            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <svg
-                class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="email-adress-icon"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@gmail.com"
-            />
-          </div>
-
-          <br />
-
-          <label
-            for="message"
-            class="block mb-2 text-sm font-medium text-gray-400 text-gray-400"
-          >
-            Your message
-          </label>
-          <textarea
-            id="message"
-            rows="4"
-            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Leave a comment..."
-          ></textarea>
-
-          <br />
-
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-          >
-            Submit
-          </button>
-        </form>
+          First Name
+        </label>
+        <input
+          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+          id="grid-first-name"
+          type="text"
+          placeholder="Jane"
+        />
+        <p class="text-red-500 text-xs italic">Please fill out this field.</p>
       </div>
-    </section>
-  );
+      <div class="w-full md:w-1/2 px-3">
+        <label
+          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          for="grid-last-name"
+        >
+          Last Name
+        </label>
+        <input
+          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          id="grid-last-name"
+          type="text"
+          placeholder="Doe"
+        />
+      </div>
+    </div>
+    <div class="flex flex-wrap -mx-3 mb-6">
+      <div class="w-full px-3">
+        <label
+          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          for="grid-password"
+        >
+          E-mail
+        </label>
+        <input
+          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          id="email"
+          type="email"
+        />
+        <p class="text-gray-600 text-xs italic">
+          Some tips - as long as needed
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-wrap -mx-3 mb-6">
+      <div class="w-full px-3">
+        <label
+          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          for="grid-password"
+        >
+          Message
+        </label>
+        <textarea
+          class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+          id="message"
+        ></textarea>
+        <p class="text-gray-600 text-xs italic">
+          Re-size can be disabled by set by resize-none / resize-y / resize-x /
+          resize
+        </p>
+      </div>
+    </div>
+    <div class="md:flex md:items-center">
+      <div class="md:w-1/3">
+        <button
+          class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+          type="button"
+        >
+          Send
+        </button>
+      </div>
+      <div class="md:w-2/3"></div>
+    </div>
+  </form>;
 }

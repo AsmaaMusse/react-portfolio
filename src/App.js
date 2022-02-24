3003faedbef573fe87b98dd7d91d3faad4ae59a1
Asmaa <Asmaa99musse@gmail.com>
2022-02-24 01:07:12 +0000

    added footer section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import "./index.css";
 
 import About from "./components/about";
-import Socials from "./components/socials";
 import Projects from "./components/projects";
 import Contacts from "./components/contacts";
 import Skills from "./components/skills";
@@ -12,7 +11,6 @@ export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <About />
-      {/* <Socials /> */}
       <Projects />
       <Skills />
       <Contacts />

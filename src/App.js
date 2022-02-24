0849b47d307630c86c3c598e03d2b9ececb399dd
Asmaa <Asmaa99musse@gmail.com>
2022-02-24 01:35:29 +0000

    Creating navbar and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import "./index.css";
 
+import Navbar from "./components/navbar";
 import About from "./components/about";
 import Projects from "./components/projects";
 import Contacts from "./components/contacts";
@@ -10,6 +11,7 @@ import Footer from "./components/footer";
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
       <About />
       <Projects />
       <Skills />
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,31 +1,27 @@
-// import logo from "../assets/images/logo.jpg";
-// import "../App.css";
+import React from "react";
+import logo from "../assets/images/logo.jpg";
+import "../App.css";
+// import { ArrowRightIcon } from "@heroicons/react/solid";
 
-// function Navbar() {
-//   return (
-//     <div>
-//       <nav>
-//         <div class="logo">
-//           <img src={logo} />
-//         </div>
-
-//         <ul id="nav-list">
-//           <li>
-//             <a href="#about-me">About Me</a>
-//           </li>
-//           <li>
-//             <a href="#projects">Projects</a>
-//           </li>
-//           <li>
-//             <a href="#contact">Contact</a>
-//           </li>
-//         </ul>
-//         <button class="hamburger" id="hamburger">
-//           <i class="fas fa-bars"></i>
-//         </button>
-//       </nav>
-//     </div>
-//   );
-// }
-
-// export default Navbar;
+export default function Navbar() {
+  return (
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <div class="logo h-10 w-10 self-center mr-2">
+          <img class="h-10 w-20 self-center" src={logo} />
+        </div>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#projects" className="mr-5 hover:text-white">
+            Projects
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Skills
+          </a>
+          <a href="#contact" className="mr-5 hover:text-white">
+            Contact Form
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}

9fe9a57435db45fc8d93bea43218fbce1a91b2c7
Asmaa <Asmaa99musse@gmail.com>
2022-05-11 13:53:10 +0100

    Corrected spelling error

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,15 +11,15 @@ export default function Projects() {
           </h1>
 
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Below are some of the projects i've created, to see more projects
-            similar to these you could view my
+            Below are some of the projects I've created, to see more projects
+            similar to these you could view my -
             <a
               href="https://github.com/AsmaaMusse"
               target="_blank"
               rel="noopener noreferrer"
               style={{ color: "#FFF" }}
             >
-              Github
+              GitHub
             </a>
           </p>
         </div>
